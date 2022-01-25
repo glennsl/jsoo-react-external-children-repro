915a3849f525eb67798d09fc545b323a4e45050f
glennsl <glenn@slotte.net>
2022-01-25 21:22:29 +0100

    repro

--- src/main.ml
+++ src/main.ml
@@ -1,3 +1,17 @@
-[@@@react.dom]
+open React.Dom.Dsl
+open Html
+module Js = Js_of_ocaml.Js
 
-let%component make () = div ~children:[React.string "Hello world from OCaml"] ()
+external%component map : children:React.element Js.js_array Js.t -> React.element
+  = {|require("react-leaflet").MapContainer|}
+
+external%component tile_layer : attribution:Js.js_string Js.t -> url:Js.js_string Js.t -> React.element
+  = {|require("react-leaflet").TileLayer|}
+
+let%component make () =
+  div [||]
+    [ map ~children:(Js.array [|
+                         tile_layer ~attribution:(Js.string "") ~url:(Js.string "") ()
+                       |])
+        ()
+    ]
